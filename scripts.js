53c3ce30124e41705e0726901c2be6ea41edc721
Alejandro <alejsant75@gmail.com>
2022-09-13 11:27:22 -0400

    1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function calcular(){
+    var dolar = document.getElementById("dolar").value;
+    var peso = document.getElementById("peso").value;
+    var ganancia = document.getElementById("ganancia").value;
+    var producto = document.getElementById("producto").value;
+
+
+
+    let precio = 
+
+    document.getElementById("precio").innerHTML = precio;
+
+}
