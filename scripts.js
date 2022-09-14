01ce1abfbedf63884bcde62801dde960507a482e
Alejandro <alejsant75@gmail.com>
2022-09-14 04:17:52 -0500

    2

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 function calcular(){
-    var dolar = document.getElementById("dolar").value;
-    var peso = document.getElementById("peso").value;
-    var ganancia = document.getElementById("ganancia").value;
-    var producto = document.getElementById("producto").value;
+    let dolar = document.getElementById("dolar").value;
+    let peso = document.getElementById("peso").value;
+    let ganancia = document.getElementById("ganancia").value;
+    let producto = document.getElementById("producto").value;
+    let margen = document.getElementById("margen").value;
 
+    let cantidadProductos = 1/peso;
+    let amortiguarDolar = parseInt(dolar) + parseInt(margen);
+    let gananciaDineroCubano = amortiguarDolar * ganancia;
+    let costoDineroCubano = dolar * producto;
 
+    let dineroCubano = costoDineroCubano + (gananciaDineroCubano/cantidadProductos);
 
-    let precio = 
-
-    document.getElementById("precio").innerHTML = precio;
+    document.getElementById("precio").innerHTML = dineroCubano;
 
 }
