895b86f2e32783a7aeab5b18eb23f947a7326fb1
Alejandro <91127175+Alejo2075@users.noreply.github.com>
2022-10-01 17:17:53 -0400

    Update scripts.js
    Jsj

--- scripts.js
+++ scripts.js
@@ -3,11 +3,9 @@ function calcular(){
     let peso = document.getElementById("peso").value;
     let ganancia = document.getElementById("ganancia").value;
     let producto = document.getElementById("producto").value;
-    let margen = document.getElementById("margen").value;
 
     let cantidadProductos = 1/peso;
-    let amortiguarDolar = parseInt(dolar) + parseInt(margen);
-    let gananciaDineroCubano = amortiguarDolar * ganancia;
+    let gananciaDineroCubano = dolar * ganancia;
     let costoDineroCubano = dolar * producto;
 
     let dineroCubano = costoDineroCubano + (gananciaDineroCubano/cantidadProductos);
